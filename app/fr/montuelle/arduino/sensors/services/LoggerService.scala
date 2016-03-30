ac7aa744e959d9cfcdd43cf204acf6d9ea4ea989
Benoit Montuelle <benoit.montuelle@april.com>
2016-03-30 14:21:31 +0200

    feat(serial) ajout d'un timestamp aux document loggués

--- app/fr/montuelle/arduino/sensors/services/LoggerService.scala
+++ app/fr/montuelle/arduino/sensors/services/LoggerService.scala
@@ -3,6 +3,7 @@ package fr.montuelle.arduino.sensors.services
 import javax.inject.Inject
 
 import akka.util.ByteString
+import org.joda.time.DateTime
 import play.api.Logger
 import play.api.libs.json.Json
 import play.modules.reactivemongo.ReactiveMongoApi
@@ -19,7 +20,7 @@ class LoggerService @Inject() (val reactiveMongoApi: ReactiveMongoApi) {
     if (txtValue.contains("|")) {
       val values: Seq[String] = txtValue.split('|').filterNot(_.length == 0)
       Logger.debug(s"data received in text : ${values}")
-      collection.insert(Json.obj("temperature" -> values(0), "humidity" -> values(1) ))
+      collection.insert(Json.obj("temperature" -> values(0), "humidity" -> values(1), "time" -> new DateTime))
     }
   }
 
